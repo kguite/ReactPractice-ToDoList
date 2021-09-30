8fd414b7cff1f9b5cfa2340bbf7f1efcc763c5af
Katie Marie <33885541+kguite@users.noreply.github.com>
2021-09-30 14:16:27 -0700

    map through data array map through data array

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./styles.css";
-
+import todosData from "./todosData";
 import TodoItem from "./components/TodoItem";
 
 function App() {
+  const singleTodo = todosData.map((item) => (
+    <TodoItem item={item} key={item.id} text={item.text} />
+  ));
   return (
     <div className="todo-list">
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+      <h3>{singleTodo}</h3>
     </div>
   );
 }
