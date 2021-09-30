931a55f5d22f3f8414a6702a8eaa0fc33d7ed80c
Katie Marie <33885541+kguite@users.noreply.github.com>
2021-09-30 13:56:25 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./styles.css";
+
+import TodoItem from "./components/TodoItem";
+
+function App() {
+  return (
+    <div className="todo-list">
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+    </div>
+  );
+}
+
+export default App;
