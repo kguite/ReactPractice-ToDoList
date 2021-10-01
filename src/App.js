53103e4072b6bd3cfacfa2a061bd00dc2b9826be
Katie Marie <33885541+kguite@users.noreply.github.com>
2021-10-01 13:52:58 -0700

    change functional component to class component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import "./styles.css";
 import todosData from "./todosData";
 import TodoItem from "./components/TodoItem";
 
-function App() {
-  const singleTodo = todosData.map((item) => (
-    <TodoItem item={item} key={item.id} text={item.text} />
-  ));
-  return (
-    <div className="todo-list">
-      <h3>{singleTodo}</h3>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todosData
+    };
+  }
 
+  render() {
+    const singleTodo = this.state.todos.map((item) => (
+      <TodoItem item={item} key={item.id} text={item.text} />
+    ));
+    return (
+      <div className="todo-list">
+        <h3>{singleTodo}</h3>
+      </div>
+    );
+  }
+}
 export default App;
