c918d0afff67467cf526fb87f5912d1cff48f15d
Katie Marie <33885541+kguite@users.noreply.github.com>
2021-10-04 12:02:09 -0700

    change component from class to function add useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 import todosData from "./todosData";
 import TodoItem from "./components/TodoItem";
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      todos: todosData
-    };
+function App() {
+  const [data, setData] = useState([]);
+
+  function handleChange(id) {
+    setData((prevState) => {
+      const items = [...prevState];
+      const itemToChange = items.find((item) => item.id === id);
+      itemToChange.completed = !itemToChange.completed;
+      return items;
+    });
   }
 
-  render() {
-    const singleTodo = this.state.todos.map((item) => (
-      <TodoItem item={item} key={item.id} text={item.text} />
-    ));
-    return (
-      <div className="todo-list">
-        <h3>{singleTodo}</h3>
-      </div>
-    );
+  async function fetchData() {
+    fetch(`fakeApiUrl`)
+      .then((res) => res.json())
+      .then((returnData) => {
+        // setData(returnData);
+      });
+    await new Promise((resolve) => {
+      setTimeout(() => resolve(setData(todosData)), 5000);
+    });
   }
+
+  function postData() {
+    fetch("fakeApiPostRoute", {
+      method: "POST",
+      body: JSON.stringify(data)
+    })
+      .then((res) => res.json())
+      .then(console.log);
+  }
+
+  const singleTodo = data.map((item) => (
+    <TodoItem
+      item={item}
+      key={item.id}
+      text={item.text}
+      handleChange={handleChange}
+    />
+  ));
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return data.length > 0 ? (
+    <div className="todo-list">
+      <h3>{singleTodo}</h3>
+      <button onClick={postData}>Save Changes to Server</button>
+    </div>
+  ) : (
+    <button onClick={fetchData}>Load Data</button>
+  );
 }
 export default App;
